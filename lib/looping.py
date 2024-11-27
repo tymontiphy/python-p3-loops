cb49d90a9e198be423f3410de8fa12a395fb6e9d
Your Name <youremail@domain.com>
2024-11-28 01:22:43 +0300

    add python loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    return [i ** 2 for i in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else :
+            print(i)
     pass
